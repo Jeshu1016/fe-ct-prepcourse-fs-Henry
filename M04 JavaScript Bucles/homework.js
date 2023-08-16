f74748c33e4b7065109e68f32d42f688e5ef363e
Jesus Alvino <jesusalvino.imei@gmail.com>
2023-08-16 07:24:57 -0300

    Terminando 4

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -123,7 +123,14 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-   
+   if (num1 > num2 && num1 > num3 && num1 > 0) return "Numero 1 es mayor y positivo";
+   else if ( num1 < 0 || num2 < 0 || num3 < 0) return "Hay negativos";
+   else if ( num3 > num1 && num3 > num2) {
+      num3 = num3 + 1;
+      return num3;
+   }
+   else if ( num1 === 0 && num2 === 0 && num3 === 0) return "Error"
+   else return false
 }
 
 function esPrimo(num) {
@@ -133,7 +140,13 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if (num <= 0 || num === 1) return false;
+   for (var i = 2; i < num; i++){
+      if (num % i === 0) return false;
+   }
+   return true
 }
+
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
